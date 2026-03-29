f535e3e86b498929f7bd81c70a0799617056edd7
Devashish <101060581+DEVASHISH23KGN@users.noreply.github.com>
2026-03-29 23:23:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,9 +9,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", event => {
